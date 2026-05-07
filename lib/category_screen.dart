7ca9d2bd1260b8e906312ba5c83bc94147a0d535
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-05-07 14:00:45 +0530

    Update category data and enhance CategoryScreen layout

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/data/category_data.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -25,14 +26,23 @@ class _CategoryScreenState extends State<CategoryScreen> {
           crossAxisSpacing: 20, // Column ke bich ka space (gap)
           mainAxisSpacing: 20, // row ke bich ka space (gap)
         ),
-        children: List.generate(20, (index) {
+        children: List.generate(category.length, (index) {
           return Container(
-            color: const Color.fromARGB(255, 13, 43, 21),
-            child: Center(
-              child: Text(
-                "list gen $index",
-                style: TextStyle(color: Colors.white),
-              ),
+            padding: EdgeInsets.all(16),
+            color: category[index].color,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      category[index].id,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(category[index].title),
+                  ],
+                ),
+              ],
             ),
           );
         }),
--- lib/data/category_data.dart
+++ lib/data/category_data.dart
@@ -4,17 +4,45 @@ import 'package:food_app/model/category_model.dart';
 List<CategoryModel> category = [
   CategoryModel(id: "A1", title: "south india", color: Colors.yellow),
 
-  CategoryModel(id: "A1", title: "north india", color: Colors.yellow),
+  CategoryModel(
+    id: "A1",
+    title: "north india",
+    color: const Color.fromARGB(255, 117, 114, 94),
+  ),
 
-  CategoryModel(id: "A2", title: "south india", color: Colors.yellow),
+  CategoryModel(
+    id: "A2",
+    title: "east india",
+    color: const Color.fromARGB(255, 20, 110, 42),
+  ),
 
-  CategoryModel(id: "A3", title: "south india", color: Colors.yellow),
+  CategoryModel(
+    id: "A3",
+    title: "west india",
+    color: const Color.fromARGB(255, 19, 128, 113),
+  ),
 
-  CategoryModel(id: "A4", title: "south india", color: Colors.yellow),
+  CategoryModel(
+    id: "A4",
+    title: "powerfull  india",
+    color: const Color.fromARGB(255, 118, 8, 125),
+  ),
 
-  CategoryModel(id: "A5", title: "south india", color: Colors.yellow),
+  CategoryModel(
+    id: "A5",
+    title: "Elephent india",
+    color: const Color.fromARGB(255, 126, 7, 25),
+  ),
 
-  CategoryModel(id: "A6", title: "south india", color: Colors.yellow),
+  CategoryModel(
+    id: "A6",
+    title: "india",
+    color: const Color.fromARGB(255, 163, 7, 101),
+  ),
 
-  CategoryModel(id: "A7", title: "south india", color: Colors.yellow),
+  CategoryModel(
+    id: "A7",
+    title: "ONly one india",
+    color: const Color.fromARGB(255, 215, 127, 3),
+  ),
 ];
